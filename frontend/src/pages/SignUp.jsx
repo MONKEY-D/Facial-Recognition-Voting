a3911874cd30ef498c7cc30c2f3a4a40a8b26cd5
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2024-11-07 21:00:06 +0530

    changes colors of buttons

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -157,7 +157,7 @@ export default function SignUp() {
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-green-500 via-green-500 to-blue-500 rounded-lg text-white">
               Let's
             </span>
             Vote
@@ -207,7 +207,7 @@ export default function SignUp() {
             </div>
             <ImageUploader onFileSelect={handleFileSelect} />
             <Button
-              gradientDuoTone="purpleToPink"
+              gradientDuoTone="greenToBlue"
               type="submit"
               disabled={loading}
             >
